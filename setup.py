444bd0e32b4566b6e572511ca0fee98aefbc45e6
Cai, Meng <mengcai24601@gmail.com>
2023-04-05 21:42:58 +0200

    upload data

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="seesus",
+    version="0.1.0",
+    description="a social, environmental, and economic sustainability classifier",
+    license="GPL-3.0",
+    author="Meng Cai",
+    author_email="mengcai24601@gmail.com",
+    packages=["seesus"],
+    install_requires=["pytest"],
+    include_package_data=True
+)
