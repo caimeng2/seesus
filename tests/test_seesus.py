8a903aac1a304dd925d040d30b90575f506a1c92
Cai, Meng <mengcai24601@gmail.com>
2023-07-04 14:37:55 +0200

    fix toml

--- tests/test_seesus.py
+++ tests/test_seesus.py
@@ -1,5 +1,5 @@
-from seesus.seesus import SeeSus
 import pytest
+from seesus.seesus import SeeSus
 
 def test_seesus():
     text = "our goal is to mitigate climate change, end poverty, and reduce inequality globally"
@@ -9,4 +9,6 @@ def test_seesus():
     see = result.see
     assert targets == ['SDG13_2', 'SDG13_general', 'SDG10_3', 'SDG1_2']
     assert sdgs == ['SDG10', 'SDG1', 'SDG13']
-    assert see == {'social_sustainability': True, 'environmental_sustainability': True, 'economic_sustainability': False}
+    assert see == {'social_sustainability': True,
+                   'environmental_sustainability': True,
+                   'economic_sustainability': False}
