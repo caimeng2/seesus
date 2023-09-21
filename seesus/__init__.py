76a2619a8f4956cf6ed7ae29b486e6646a034628
Cai, Meng <mengcai24601@gmail.com>
2023-09-21 14:10:18 +0200

    init doc

--- seesus/__init__.py
+++ seesus/__init__.py
@@ -1,2 +1,12 @@
-"""seesus help message coming up"""
+"""
+seesus
+-----------------------------------------------------------------------
+A Python tool for checking whether a textual expression aligns with sustainability as defined by the United Nations Sustainable Development Goals.
+
+__version__ = "1.0.0"
+__title__ = "seesus"
+__url__ = "https://github.com/caimeng2/seesus"
+__license__ = "LGPL-3.0"
+"""
+
 from seesus.seesus import SeeSus
