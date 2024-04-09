0adeebdc306d8bebfc4508535c049f25f5a68dee
Cai, Meng <mengcai24601@gmail.com>
2024-04-09 22:39:25 +0200

    update docs

--- src/seesus/__init__.py
+++ src/seesus/__init__.py
@@ -1,12 +1,11 @@
 """
 seesus
 ------
-A Python tool for checking whether a textual expression aligns with sustainability as defined by the United Nations Sustainable Development Goals.
+a social, environmental, and economic sustainability classifier
 
-__version__ = "1.0.0"
 __title__ = "seesus"
 __url__ = "https://github.com/caimeng2/seesus"
-__license__ = "LGPL-3.0"
+__license__ = "GPL-3.0"
 """
 
 from seesus.seesus import SeeSus
